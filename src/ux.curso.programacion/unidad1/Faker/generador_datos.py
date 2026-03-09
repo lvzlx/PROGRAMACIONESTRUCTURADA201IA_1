dc16b3f67ea67e61cd77b96400dfb13616ec6446
Luz <luzmariarobles18@gmail.com>
2026-03-09 07:43:12 -0600

    Ejercicio Faker

---
+++ src/ux.curso.programacion/unidad1/Faker/generador_datos.py
@@ -0,0 +1,10 @@
+#importar la libreria faker
+from faker import Faker
+faker= Faker("es_MX")
+
+print("Generando datos Dummy con Faker")
+
+print(f"Nombre:{faker.name()}")
+print(f"Direccion:{faker.address()}")
+print(f"Telefono:{faker.phone_number()}")
+print(f"Correo:{faker.email()}")
