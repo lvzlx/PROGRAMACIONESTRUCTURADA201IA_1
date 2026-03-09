bdb5387cccbb19d9f5a19a5b1d5c917a3b09134c
Luz <luzmariarobles18@gmail.com>
2026-03-09 08:02:25 -0600

    generador_ciudades

---
+++ src/ux.curso.programacion/unidad1/Faker/generador_ciudades.py
@@ -0,0 +1,14 @@
+from faker import Faker
+fake = Faker('es_MX')
+
+#1. Declaracion de un vector vacio
+ciudades_ia = []
+
+#2. Operacion de llenado [Ciclo]
+for _ in range(5):
+        ciudades_ia.append(fake.city())
+
+#3. Escritura de arreglos (Mostrar resultados)
+print("\n--- DATASET DE CIUDADES GENERADO ---")
+for i in range(len(ciudades_la)):
+    print(f"Registro {i+1}: {ciudades_ia[i]}")
