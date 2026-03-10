9d75bda8e3009ff95151e6a81037feb4366df860
Luz <luzmariarobles18@gmail.com>
2026-03-10 08:40:43 -0600

    comentarioss

---
+++ src/ux.curso.programacion/unidad1/ejercicios_induccion/calificaciones.py
@@ -0,0 +1,13 @@
+#Una varuable guardará la calificación obtenida
+
+#Definicion de la funcion main (Controla el flujo del programa)
+def main():
+    print("Bienvenido al contador de positivos")
+    contador_positivos()
+
+"""
+Ejecuta la función para que realice lo que se tiene programado
+"""    
+#LLamada de la funcion main para iniciar el programa
+if __name__ == "__main__":
+    main()
