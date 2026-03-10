39721c432d5deb117c9d04edb8c8e4abcf582488
Luz <luzmariarobles18@gmail.com>
2026-03-10 09:25:56 -0600

    avance calificaciones

--- src/ux.curso.programacion/unidad1/ejercicios_induccion/calificaciones.py
+++ src/ux.curso.programacion/unidad1/ejercicios_induccion/calificaciones.py
@@ -1,10 +1,25 @@
-#Una variable guardará la calificación obtenida
+#Una variable guardará la calificacion obtenida
 
 #Definicion de la funcion main (Controla el flujo del programa)
 def main():
-    print("Bienvenido al contador de positivos")
-     #Aquí irá la función para designar las calificaciones
+    while True:
+        n= float(input("Ingrese una calificacion de 0 a 100:\n "))
+        if n >= 0 and n <= 100:
+            break
+    else:
+        print("Numero fuera del rango. Intenta nuevamente.")
 
+    if n >= 90:
+            grado= "A"
+    elif n >= 80:
+            grado= "B"
+    elif n >= 70:
+            grado= "C"
+    elif n >= 69:
+            grado= "D"
+    else:
+            grado = "F"
+    print("El grado es:", grado)
 
 #LLamada de la funcion main para iniciar el programa
 if __name__ == "__main__":
