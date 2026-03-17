727d1a656a79b3233d32bfd358b5a090d3d6b302
Luz <luzmariarobles18@gmail.com>
2026-03-17 07:30:36 -0600

    ejercicios_aritmeticos

---
+++ src/ux.curso.programacion/unidad1/ejercicio_aritmeticos/ejercicios_aritmeticos.py
@@ -0,0 +1,32 @@
+#ejemplo de ejercicicios aritmeticos
+
+def mostrar_operadores():
+    #suma
+    suma = 5 + 3
+    print("suma: 5 + 3 = ", suma)
+
+    #resta
+    resta = 10 - 4
+    print("resta = 10 - 4 = ", resta)
+
+    #multiplicacion
+    multiplicacion = 6 * 7
+    print("multiplicacion = 6 * 7 = ", multiplicacion)
+
+    #division entera 
+    division_entera = 10 // 3
+    print("division_entera = 10 // 3 = ", division_entera)
+
+    #modulo
+    modulo = 10 % 3
+    print("modulo = 10 % 3 = ", modulo)
+
+    #potenciacion
+    potenciacion = 2 ** 3
+    print("potenciacion = 2 ** 3 = ", potenciacion)
+
+def main():
+        mostrar_operadores()
+
+if __name__ == "__main__":
+        main()
