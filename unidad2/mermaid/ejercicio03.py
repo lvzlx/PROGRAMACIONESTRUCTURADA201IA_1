ca582bfde7c835a78780d04b611d0d13728e3aae
Luz <luzmariarobles18@gmail.com>
2026-04-06 08:23:43 -0600

    ejercicio03

---
+++ unidad2/mermaid/ejercicio03.py
@@ -0,0 +1,12 @@
+"""
+"""
+
+N = int(input("Ingresa la cantidad de números impares: "))
+
+contador = 0
+numero = 1
+
+while contador < N:
+    print(numero)
+    numero = numero + 2
+    contador = contador + 1
