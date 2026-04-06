ddc2e4178ddd4ba663fdc37b827654bcef41d66b
Luz <luzmariarobles18@gmail.com>
2026-04-06 08:24:59 -0600

    ejercicio03

--- unidad2/mermaid/ejercicio03.py
+++ unidad2/mermaid/ejercicio03.py
@@ -1,11 +1,10 @@
-"""
-"""
-
 N = int(input("Ingresa la cantidad de números impares: "))
 
 contador = 0
 numero = 1
 
+# este ciclo se repite mientras el contador sea menor que N
+# se ejecuta hasta imprimir la cantidad de números que pidio el usuario
 while contador < N:
     print(numero)
     numero = numero + 2
