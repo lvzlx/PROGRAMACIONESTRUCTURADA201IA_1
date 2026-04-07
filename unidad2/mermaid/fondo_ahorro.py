19765f2a03068005b8fbe57160f5cabfd2d4b70e
Luz <luzmariarobles18@gmail.com>
2026-04-07 07:48:51 -0600

    ejercicio06fondoahorro

---
+++ unidad2/mermaid/fondo_ahorro.py
@@ -0,0 +1,17 @@
+#implementar en python una simulación de un fondo de ahorro. El usuario realiza depósitos mensuales de forma sucesiva hasta que la suma total iguala o supera una meta económica preestablecida de $1,000.
+
+def fondo_ahorro():
+    
+    saldo = 0
+    meta = 1000
+    while saldo < meta:
+        deposito = float(input("Ingrese el monto del depósito mensual: "))
+        saldo += deposito
+    return saldo
+
+def main():
+    resultado = fondo_ahorro()
+    print("Has alcanzado tu meta de ahorro con un saldo de:", resultado)
+
+if __name__ == "__main__":
+    main()
