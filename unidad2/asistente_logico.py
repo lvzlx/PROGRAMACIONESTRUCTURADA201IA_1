f2b22ed6e9f9b7e21445131099653e52be67ef3a
Luz <luzmariarobles18@gmail.com>
2026-04-07 18:44:44 -0600

    asistente_logico

--- unidad2/asistente_logico.py
+++ unidad2/asistente_logico.py
@@ -5,34 +5,33 @@ from datetime import datetime
 nombre_asistente = "quikin"
 
 """
-en esta funcion se llevan a cabo todas las estructuras condicionales 
+en esta funcion se llevan a cabo todas las estructuras condicionales
 con el proposito de identificar la intencion del usuario.
 """
 
 def ejecutar_asistente():
     #mensaje de bienvenida
-    print(f"te doy la bienvenida, soy {nombre_asistente}, tu asistente y estoy listo para ayudarte.")
+    print(f"te doy la bienvenida, soy{nombre_asistente}, tu asistente y estoy listo para ayudarte.")
 
     #entrada de datos
-    frase = input("¿en que te puedo ayudar hoy?: ").lower()
+    frase= input("¿en que te puedo ayudar hoy?: ").lower()
 
-    #clasificacion de intencion
+    #clasificacion de intencion 
     if "hola" in frase or "buenos días" in frase:
         print("¡Hola! Soy tu asistente, es un gusto saludarte.")
-
+    
     elif "clima" in frase or "temperatura" in frase:
-        print("consultando el servicio meteorologico... hoy en Xalapa tendremos un día nublado.")
-
+        print("consultando el servicio meteorologico... hoy en Xalapa tendremos un dia nublado")
+    
     elif "hora" in frase or "tiempo" in frase:
         hora_actual = datetime.now().strftime("%I:%M %p")
         print(f"la hora actual del sistema es: {hora_actual}")
 
     else:
-        print("lo siento, todavía no entiendo ese comando, ¿podrías intentar con otra palabra?")
+        print("lo siento, todavia no entiendo ese comando, ¿podrias intentar con otra palabra?")
 
     #despedida
-    print(f"proceso finalizado, gracias tiline por usar {nombre_asistente}.")
-
+    print(f"proceso finalizado, gracias tiline por usar{nombre_asistente}.")
 
 
 def main():
@@ -40,3 +39,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
