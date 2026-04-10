af43afaff2db5390c9f6850472aa46c671a4dd72
Luz <luzmariarobles18@gmail.com>
2026-04-10 08:55:41 -0600

    identidad_trigonometrica

--- unidad2/jupiter_ejercicio_01/identidad_trigonometrica.py
+++ unidad2/jupiter_ejercicio_01/identidad_trigonometrica.py
@@ -8,7 +8,7 @@ def identidad_trigonometrica(x):
     identidad = seno_x + coseno_x
     print(f"Para x = {x} grados, sin^2(x) + cos^2(x) = {identidad}")
 
-def identidad_trigonometrica(x):
+def identidad_trigonometrica_2(x):
     numero_radianes = math.radians(x)
     seno_x = math.pow(math.sin(numero_radianes), 2)
     coseno_x = math.pow(math.cos(numero_radianes), 2)
@@ -18,6 +18,7 @@ def identidad_trigonometrica(x):
 def main():
     x = int(input("Ingrese el valor de x en grados: "))
     identidad_trigonometrica(x)
+    identidad_trigonometrica_2(x)
 
 if __name__ == "__main__":
     main()
